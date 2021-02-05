b8f15fce833733460d708a8c0335d6901020c716
Tim_Tan <31795244+TimSnake13@users.noreply.github.com>
2021-02-05 20:50:39 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+// Data From https://www.officetimeline.com/blog/stephen-hawking-timeline
+export const data = [
+  {
+    time: "1942",
+    content: "Stephen Hawking is born"
+  },
+  {
+    time: "1952",
+    content: "Enrolls at St. Albans School"
+  },
+  {
+    time: "1959",
+    content: "Begins studying physics at Oxford"
+  },
+  {
+    time: "1962",
+    content: "Starts graduate work at Cambridge"
+  },
+  {
+    time: "1963",
+    content: "Diagnosed with ALS"
+  },
+  {
+    time: "1965",
+    content: "Marries Jane Wilde"
+  },
+  {
+    time: "1966",
+    content: "Receives his PhD at age 23"
+  },
+  {
+    time: "1970",
+    content: "Discovers the second law of black hole dynamics"
+  },
+  {
+    time: "1974",
+    content: "Elected as a fellow of the Royal Society"
+  },
+  {
+    time: "1975",
+    content: "Publishes paper on Hawking Radiation",
+    highLight: true
+  },
+  {
+    time: "1970",
+    content: "Discovers the second law of black hole dynamics"
+  },
+  {
+    time: "1970",
+    content: "Discovers the second law of black hole dynamics"
+  },
+  {
+    time: "1970",
+    content: "Discovers the second law of black hole dynamics"
+  },
+  {
+    time: "1970",
+    content: "Discovers the second law of black hole dynamics"
+  },
+  {
+    time: "1970",
+    content: "Discovers the second law of black hole dynamics"
+  }
+];
