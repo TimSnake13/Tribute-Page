d25d5a9026b3b8949ba347ca6bc5ac4ee132c4d0
Tim_Tan <31795244+TimSnake13@users.noreply.github.com>
2021-02-05 21:16:07 +0800

    Added TODOs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./styles.css";
 import data from "./data.js";
 
+// TODO:
+// 1. Display all the data on page
+// 2. Scroll element one by one, snap to center
+// 3. Center element can change style
+
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
