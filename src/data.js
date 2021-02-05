3716fbbe94e3c997fb737ea92cf30d014386a487
Tim_Tan <31795244+TimSnake13@users.noreply.github.com>
2021-02-05 20:58:48 +0800

    Stephen Hawking Timeline Data

--- src/data.js
+++ src/data.js
@@ -21,10 +21,6 @@ export const data = [
     content: "Diagnosed with ALS"
   },
   {
-    time: "1965",
-    content: "Marries Jane Wilde"
-  },
-  {
     time: "1966",
     content: "Receives his PhD at age 23"
   },
@@ -42,23 +38,45 @@ export const data = [
     highLight: true
   },
   {
-    time: "1970",
-    content: "Discovers the second law of black hole dynamics"
+    time: "1979",
+    content: "Becomes Lucasian Professor at Cambridge"
   },
   {
-    time: "1970",
-    content: "Discovers the second law of black hole dynamics"
+    time: "1983",
+    content: "Publishes the Hartle-Hawking State model",
+    highLight: true
   },
   {
-    time: "1970",
-    content: "Discovers the second law of black hole dynamics"
+    time: "1988",
+    content: "A Brief History of Time is published",
+    highLight: true
   },
   {
-    time: "1970",
-    content: "Discovers the second law of black hole dynamics"
+    time: "2001",
+    content: "The Universe in a Nutshell is published",
+    highLight: true
   },
   {
-    time: "1970",
-    content: "Discovers the second law of black hole dynamics"
+    time: "2005",
+    content: "Publishes Information Loss in Black Holes",
+    highLight: true
+  },
+  {
+    time: "2009",
+    content: "Receives the Presidential Medal of Freedom",
+    highLight: true
+  },
+  {
+    time: "2010",
+    content: "The Grand Design is published",
+    highLight: true
+  },
+  {
+    time: "2011",
+    content: "Participates in a trial for iBrain"
+  },
+  {
+    time: "2018",
+    content: "Dis peacefully in his home"
   }
 ];
